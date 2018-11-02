3a1addc1eb27c046b8b3c7e24c7f681bbe3f2be2
ryanlecoutresparta <42863583+ryanlecoutresparta@users.noreply.github.com>
2018-11-02 13:59:45 +0000

    Update default_spec.rb

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -11,7 +11,7 @@ describe 'nginx-cookbook::default' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
 
